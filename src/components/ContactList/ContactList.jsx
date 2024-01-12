3761e9f077c796bcc7a5109536e4e6189e70f0af
Олександр Канюка <137859612+crusaider423@users.noreply.github.com>
2024-01-12 21:43:26 +0200

    update

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -6,14 +6,15 @@ export const ContactList = ({ contacts, deleteContact }) => {
       (name || number) && (
         <li key={id}>
           {name}: {number}
-          <button className={css.btn} type="button" onClick={() => deleteContact(id)}>
+          <button
+            className={css.btn}
+            type="button"
+            onClick={() => deleteContact(id)}
+          >
             delete
           </button>
         </li>
       )
   );
-  // if (elem[0] === '') {
-  //   elem.splice(0, 1);
-  // }
   return <ol> {elem}</ol>;
 };
